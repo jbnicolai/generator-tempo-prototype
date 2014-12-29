516b895e1686d108e8f231912f3a432dd69a7d80
Arnþór Snær Sævarsson <arnthorsnaer@tmsoftware.is>
2014-12-29 16:19:41 +0000

    generator improvements
    Added projectName and entity user input to the generator, get’s added
    package.json and bower.son to be generated.
    
    Also added readme.json to be generated.

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
+var _s = require('underscore.string');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -13,49 +15,47 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the fantastic' + chalk.red('TempoPrototype') + ' generator!'
+      'Ahoy! Let us generate a ' + chalk.green('Tempo UI Prototype') + '!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type    : 'input',
+      name    : 'projectName',
+      message : 'What is your project name?',
+      default : 'A Team Report' // Default to current folder name
     },{
-      type: 'confirm',
-      name: 'anotherOption',
-      message: 'Would you like to enable this another option?',
-      default: true
-    }];
+      type: "list",
+      name: "entity",
+      message: "Which entity are you designing for?",
+      choices: [
+        "User",
+        "Team",
+        "Program",
+        "Account",
+        "Customer",
+        "Advanced"
+        ]
+      }];
 
     this.prompt(prompts, function (props) {
       this.someOption = props.someOption;
-      this.anotherOption = props.anotherOption;
+      this.projectName = props.projectName;
+      this.entity = props.entity;
+      this.log(String(this.anotherOption));
       done();
     }.bind(this));
   },
 
   writing: {
+
     app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+      this.template('_package.json', 'package.json');
+      this.template('_bower.json', 'bower.json');
+      this.template('README.md', 'README.md');
     },
 
     projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
+      this.copy('editorconfig', '.editorconfig');
     }
   },
 
---
+++ app/templates/README.md
@@ -0,0 +1,3 @@
+# <%= entity %> : <%= projectName %> - A Tempo UI Prototype
+
+This is a UI Prototype for the <%= projectName %> project. It was generated for the <%= entity %> entity.
